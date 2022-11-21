b6141bcd4c968d8db99dcd2e7b2edc4d39789a9c
Luka Signe--Morice <luka@signemorice.com>
2022-11-21 22:25:14 +0100

    Retreive & display top artist / track OK

--- client/src/stores/store.js
+++ client/src/stores/store.js
@@ -46,3 +46,39 @@ export const selected = reactive({
         this.tracks = a
     },
 })
+
+export const token = reactive({
+    accessToken: null,
+    refreshToken: null,
+    expiresIn: null,
+    timestamp: null,
+
+    setAccessToken(newAccessToken) {
+        this.accessToken = newAccessToken;
+    },
+    setRefreshToken(newRefreshToken) {
+        this.refreshToken = newRefreshToken;
+    },
+    setExpiresIn(newExpirationTime) {
+        this.expiresIn = newExpirationTime;
+    },
+    setTimestamp(newTimestamp) {
+        this.timestamp = newTimestamp;
+    }
+})
+
+
+export const LOCALSTORAGE_KEYS = {
+    accessToken: 'spotify_access_token',
+    refreshToken: 'spotify_refresh_token',
+    expireTime: 'spotify_token_expire_time',
+    timestamp: 'spotify_token_timestamp',
+}
+
+// Map to retrieve localStorage values
+export const LOCALSTORAGE_VALUES = {
+    accessToken: window.localStorage.getItem(LOCALSTORAGE_KEYS.accessToken),
+    refreshToken: window.localStorage.getItem(LOCALSTORAGE_KEYS.refreshToken),
+    expireTime: window.localStorage.getItem(LOCALSTORAGE_KEYS.expireTime),
+    timestamp: window.localStorage.getItem(LOCALSTORAGE_KEYS.timestamp),
+};
